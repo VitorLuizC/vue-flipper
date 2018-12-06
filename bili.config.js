2cb9042bd3762038c68e54040cfe39e43d5201f3
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-12-06 13:32:32 -0200

    :wrench: setup bili to generate CommonJS, ESM, UMD and CSS bundles

---
+++ bili.config.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const vue = require('rollup-plugin-vue').default;
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  input: 'src/Flipper.vue',
+  format: ['es', 'cjs', 'umd'],
+  plugins: [
+    vue({
+      css: false,
+      style: {
+        trim: false,
+        postcssPlugins: [
+          autoprefixer({ browsers: ['> 0.00001%'] })
+        ]
+      }
+    })
+  ]
+};
